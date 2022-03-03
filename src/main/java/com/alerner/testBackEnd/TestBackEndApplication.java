8096ebe75c097eaf6cc7da2c1f26d2ad893b9c64
Andres Lerner <andreslerner99@gmail.com>
2022-03-03 14:07:56 -0300

    Configuraciones y dependencias para el desarrollo del login con Spring security y JWT

--- src/main/java/com/alerner/testBackEnd/TestBackEndApplication.java
+++ src/main/java/com/alerner/testBackEnd/TestBackEndApplication.java
@@ -1,8 +1,15 @@
 package com.alerner.testBackEnd;
 
+import com.alerner.testBackEnd.filterJWT.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,5 +20,6 @@ public class TestBackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestBackEndApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/com/alerner/testBackEnd/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.alerner.testBackEnd.utils;
+
+public class Constants
+{
+    public static final String JWT_HEADER = "Authorization";
+    public static final String JWT_PREFIX = "Bearer ";
+    public static final String JWT_SECRET = "testBackEnd";
+    public static final String AUTH_ADMIN = "ADMIN";
+}
