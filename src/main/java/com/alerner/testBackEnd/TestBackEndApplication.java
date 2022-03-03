b97d4d67228d1ff46da468c9d94694d4c6423b08
Andres Lerner <andreslerner99@gmail.com>
2022-03-03 00:02:20 -0300

    Configuración y dependencia de Swagger

--- src/main/java/com/alerner/testBackEnd/TestBackEndApplication.java
+++ src/main/java/com/alerner/testBackEnd/TestBackEndApplication.java
@@ -2,8 +2,12 @@ package com.alerner.testBackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ConfigurationPropertiesScan("com.alerner.testBackEnd.configuration")
 public class TestBackEndApplication {
 
 	public static void main(String[] args) {
