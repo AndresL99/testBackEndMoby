ba0cce7f5e0625ecabde28b615c2185dcb87cf67
Andres Lerner <andreslerner99@gmail.com>
2022-03-03 01:14:37 -0300

    Se agregaron anotaciones de log para mejorar la vista de errores.

--- src/main/java/com/alerner/testBackEnd/controller/CandidateController.java
+++ src/main/java/com/alerner/testBackEnd/controller/CandidateController.java
@@ -2,9 +2,16 @@ package com.alerner.testBackEnd.controller;
 
 import com.alerner.testBackEnd.domain.Candidate;
 import com.alerner.testBackEnd.domain.Technology;
+import com.alerner.testBackEnd.exception.CandidateExistException;
 import com.alerner.testBackEnd.exception.CandidateForTechnologyExistException;
+import com.alerner.testBackEnd.exception.CandidateNotExistException;
+import com.alerner.testBackEnd.exception.TechnologyExistException;
 import com.alerner.testBackEnd.service.CandidateService;
 import com.alerner.testBackEnd.service.TechnologyService;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,11 +25,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api/candidate")
+@Log4j2
 public class CandidateController
 {
+
     private CandidateService candidateService;
     private TechnologyService technologyService;
 
@@ -34,13 +44,31 @@ public class CandidateController
     @PostMapping
     public ResponseEntity<Candidate>addCandidate(@RequestBody Candidate candidate)
     {
-        return new ResponseEntity<>(candidateService.addCandidate(candidate), HttpStatus.CREATED);
+        try
+        {
+            return new ResponseEntity<>(candidateService.addCandidate(candidate), HttpStatus.CREATED);
+        }
+        catch (CandidateExistException ex)
+        {
+            log.error(ex.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @GetMapping("/{idCandidate}")
     public ResponseEntity<Candidate>getCandidateById(@PathVariable Long idCandidate)
     {
-        return ResponseEntity.ok(candidateService.getCandidateById(idCandidate));
+        try
+        {
+            return ResponseEntity.ok(candidateService.getCandidateById(idCandidate));
+        }
+        catch (CandidateNotExistException e)
+        {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
     @GetMapping()
@@ -64,7 +92,15 @@ public class CandidateController
     @PutMapping("/{idCandidate}/technologies/{idTechnology}")
     public ResponseEntity<Candidate>addTechnologyAndCandidate(@PathVariable Long idCandidate, @PathVariable Long idTechnology, @RequestParam Integer yearsOfExperience)
     {
-        return new ResponseEntity<Candidate>(candidateService.addTechnologyAndCandidate(idCandidate,idTechnology,yearsOfExperience),HttpStatus.OK);
+        try
+        {
+            return new ResponseEntity<Candidate>(candidateService.addTechnologyAndCandidate(idCandidate,idTechnology,yearsOfExperience),HttpStatus.OK);
+        }
+        catch (CandidateForTechnologyExistException | TechnologyExistException | CandidateExistException exception)
+        {
+            log.error(exception.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/technologies/{name}")
--- src/main/java/com/alerner/testBackEnd/controller/TechnologyController.java
+++ src/main/java/com/alerner/testBackEnd/controller/TechnologyController.java
@@ -1,7 +1,10 @@
 package com.alerner.testBackEnd.controller;
 
 import com.alerner.testBackEnd.domain.Technology;
+import com.alerner.testBackEnd.exception.TechnologyExistException;
+import com.alerner.testBackEnd.exception.TechnologyNotExistException;
 import com.alerner.testBackEnd.service.TechnologyService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/technology")
+@Log4j2
 public class TechnologyController
 {
     private TechnologyService technologyService;
@@ -28,13 +32,31 @@ public class TechnologyController
     @PostMapping
     public ResponseEntity<Technology> addTechnology(@RequestBody Technology technology)
     {
-        return new ResponseEntity<>(technologyService.addTechnology(technology), HttpStatus.CREATED);
+        try
+        {
+            return new ResponseEntity<>(technologyService.addTechnology(technology), HttpStatus.CREATED);
+        }
+        catch (TechnologyExistException e)
+        {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @GetMapping("/{idTechnology}")
     public ResponseEntity<Technology> getTechnologyById(@PathVariable Long idTechnology)
     {
-        return ResponseEntity.ok(technologyService.getTechnologyById(idTechnology));
+        try
+        {
+            return ResponseEntity.ok(technologyService.getTechnologyById(idTechnology));
+        }
+        catch (TechnologyNotExistException e)
+        {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
     @GetMapping
