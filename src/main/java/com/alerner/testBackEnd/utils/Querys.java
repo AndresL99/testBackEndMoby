d3dda924493c828f1039bfb6d8c4daf87cba4f36
Andres Lerner <andreslerner99@gmail.com>
2022-03-03 21:06:28 -0300

    Modificaciones

--- src/main/java/com/alerner/testBackEnd/utils/Querys.java
+++ src/main/java/com/alerner/testBackEnd/utils/Querys.java
@@ -7,7 +7,7 @@ public class Querys
         throw new IllegalStateException("Utility class");
     }
 
-    public static final String CANDIDATE_FOR_TECHNOLOGY = "SELECT c.id_candidate, c.type_of_document, c.first_name,c.last_name , c.document, c.date_of_birth, t.name, cft.years_of_experience FROM candidate_for_technology AS cft " +
+    public static final String CANDIDATE_FOR_TECHNOLOGY = "SELECT c.id_candidate ,t.version,c.type_of_document, c.first_name,c.last_name , c.document, c.date_of_birth, t.name, cft.years_of_experience FROM candidate_for_technology AS cft " +
             "INNER JOIN technology AS t ON cft.id_technology = t.id_technology " +
             "INNER JOIN candidate AS c ON cft.id_candidate = c.id_candidate " +
             "WHERE t.name like '%?1%'";
